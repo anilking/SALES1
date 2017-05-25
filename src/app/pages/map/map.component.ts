6b3f2ccafac6973df7fd3b44b8569e56f3a8efe7
anilpathuri <anil.pathuri@walkingtree.tech>
2017-05-25 08:39:35 +0530

    added the mouse over popup on arrow.

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -8,9 +8,9 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-public map:any;
-public seedsData: any = [];
-  constructor(public mapservice:MapserviceService) { }
+  public map: any;
+  public seedsData: any = [];
+  constructor(public mapservice: MapserviceService) { }
 
   ngOnInit() {
     this.map = L.map('map')
@@ -22,90 +22,75 @@ public seedsData: any = [];
       attribution: '<a href="http://openstreetmap.org">Open Street Map</a>',
       maxZoom: 18,
       accessToken: 'pk.eyJ1IjoiYW5pbHBhdGh1cmkiLCJhIjoiY2oybDhmcWF0MDAwMDJxcWtzMDgwZWI3cyJ9.hzryXsu_ec_AafR-QzzVUQ',
-       id: 'mapbox.light',
+      id: 'mapbox.light',
     }).addTo(this.map)
-    
+
     this.getSeedsData();
 
   }
 
-getSeedsData(){
-  this.mapservice.getSeedsData()
-		.subscribe(
-			data => {
+  getSeedsData() {
+    this.mapservice.getSeedsData()
+      .subscribe(
+      data => {
         this.seedsData = data;
         this.getMapData();
-			},
-			error => {
+      },
+      error => {
         console.log(error)
-			}
-		);
-}
+      }
+      );
+  }
 
-	getMapData(){
-		this.mapservice.getMapData()
-		.subscribe(
-			data => {
+  getMapData() {
+    this.mapservice.getMapData()
+      .subscribe(
+      data => {
         let key;
 
         let rightIcon = L.icon({
-            iconUrl: "assets/images/right.png",
-            iconRetinaUrl: "assets/images/right.png",
-            iconSize: [29, 24],
-            iconAnchor: [9, 21],
-            popupAnchor: [0, -14]
+          iconUrl: "assets/images/right.png",
+          iconRetinaUrl: "assets/images/right.png",
+          iconSize: [29, 24],
+          iconAnchor: [9, 21],
+          popupAnchor: [0, -14]
         });
         let leftIcon = L.icon({
-            iconUrl: "assets/images/left.png",
-            iconRetinaUrl: "assets/images/left.png",
-            iconSize: [29, 24],
-            iconAnchor: [9, 21],
-            popupAnchor: [0, -14]
+          iconUrl: "assets/images/left.png",
+          iconRetinaUrl: "assets/images/left.png",
+          iconSize: [29, 24],
+          iconAnchor: [9, 21],
+          popupAnchor: [0, -14]
         });
-
         for (key in data) {
-            if (data.hasOwnProperty(key)) {
-              let selectedSeed =  this.seedsData[key] || {}; 
-              let myIcon = rightIcon;
-              if(selectedSeed.fips == key){
-                if(selectedSeed && selectedSeed.change > 0){
-                  myIcon = rightIcon;
-                }
-                else{
-                    myIcon = leftIcon;                
-                }
-                L.marker( [data[key].lat, data[key].lon],{icon: myIcon} )
-                        .bindPopup( data[key].county )
-                        .addTo( this.map );
+          if (data.hasOwnProperty(key)) {
+            let selectedSeed = this.seedsData[key] || {};
+            let myIcon = rightIcon;
+            if (selectedSeed.fips == key) {
+              if (selectedSeed && selectedSeed.change > 0) {
+                myIcon = rightIcon;
+	
               }
+              else {
+                myIcon = leftIcon;
+              }
+              L.marker([data[key].lat, data[key].lon], { icon: myIcon })
+                .bindPopup('<b>' + data[key].county + '</b> </br>' + '2016 : ' + this.seedsData[key].nps2016 + ' </br>' + '2015 : ' + this.seedsData[key].nps2015)
+                .on('mouseover', function (e) {
+                  this.openPopup();
+                })
+                .on('mouseout', function (e) {
+                  this.closePopup();
+                })
+                .addTo(this.map);
             }
+          }
         }
-			},
-			error => {
+
+      },
+      error => {
         console.log(error)
-			}
-		);
-	}
-
- 
-
-style(feature) {
-  console.log(this);
-    return {
-        fillColor: feature.properties.density > 1000 ? '#800026' :
-           feature.properties.density > 500  ? '#BD0026' :
-           feature.properties.density > 200  ? '#E31A1C' :
-           feature.properties.density > 100  ? '#FC4E2A' :
-           feature.properties.density > 50   ? '#FD8D3C' :
-           feature.properties.density > 20   ? '#FEB24C' :
-           feature.properties.density > 10   ? '#FED976' :
-                      '#FFEDA0',
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-
+      }
+      );
+  }
 }
